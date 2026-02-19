75c1157278f8b0f7f500dcaf94816f629248b3d8
banegasb1247 <banegasb1247@student.faytechcc.edu>
2026-02-19 15:07:31 -0800

    Add files via upload

---
+++ P1LAB1_BanegasBrandon.py
@@ -0,0 +1,9 @@
+# Brandon Banegas
+# 02/19/2026
+# P1LAB1 - Input and Output Basics
+# This program asks the user for their first and last name and displays a welcome message.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
